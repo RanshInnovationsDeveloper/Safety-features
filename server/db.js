4347828cfc6eb0d123eca82f3b70eb7cac476531
Yash Raj Gupta <yashraj.guptadev@gmail.com>
2024-05-14 14:07:35 +0530

    database change

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 
 exports.dbConnection =async()=>{
     mongoose.set('strictQuery',true)
-    await mongoose.connect('mongodb+srv://anideepkalia71:anideepkalia71@cluster0.4bgy5md.mongodb.net/')
+    await mongoose.connect('mongodb+srv://shubhamsingalransh:Abc123@cluster0.4bgy5md.mongodb.net/')
         .then(()=>{
             console.log('Db Connected')
         }).catch((err)=>{
