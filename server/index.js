cd00f84c99651957b6dcd08fbb017657febd0a25
Anideep <anideepkalia@gmail.com>
2024-05-26 03:03:27 +0530

    documentation of backend and corrected worker

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const { dbConnection } = require('./db');
 var cors = require('cors')
+const swaggerUi = require('swagger-ui-express');
+const { openapispec } = require('./spec');
 const dotenv = require('dotenv')
 
 dotenv.config();
@@ -17,7 +19,8 @@ app.use(cors())
 
 app.use('/api/user',require('./routes/user'))      
 app.use('/api/admin',require('./routes/admin'))      
-app.use('/api/place',require('./routes/place'))      
+app.use('/api/place',require('./routes/place'))   
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(openapispec));   
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
