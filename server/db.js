c81d0c01e5a3b78ed4d8ae06d5dab9c8f0c24738
Anideep Kalia <128630418+Anideep-Kalia@users.noreply.github.com>
2024-07-11 14:23:53 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 
 exports.dbConnection =async()=>{
     mongoose.set('strictQuery',true)
-    const mongourl = 'mongodb+srv://anideepkalia71:anideepkalia71@cluster0.4bgy5md.mongodb.net/';
+    const mongourl = 'mongodb+srv://shubhamsingalransh:Abc123@cluster0.4bgy5md.mongodb.net/';
     await mongoose.connect(mongourl)
         .then(()=>{
             console.log('Db Connected')
